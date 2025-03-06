b3929cb564f540c6803078650cb133ee0d4d8322
william <williamwei9707@gmail.com>
2025-03-06 18:05:26 +0800

    begin to work on GEMM and GPT2.

---
+++ cu/sgemm/src/kernels/naive.cuh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+
+/*
+
+Matrix sizes:
+MxK * KxN = MxN
+
+*/
+
+__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
+                            const float *B, float beta, float *C) {
+  const auto x = blockIdx.x * blockDim.x + threadIdx.x;
+  const auto y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (x < N && y < M) {
+    float tmp = 0.0;
+    for (int i = 0; i < K; i++) {
+      tmp += A[x * K + i] * B[i * N + y]
+    }
+    // C = \alpha * (A @ B) + \beta * C
+    C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+  }
+}
--- nn/swim_transformer/model.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-
-import numpy as np
-
